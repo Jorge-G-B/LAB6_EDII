f60aa8a1ca2ce6f04b26c0a9e99acfad34f60bab
Bushinii <jorgeg.baldizon@gmail.com>
2020-11-12 00:19:58 -0600

    RSA Encryptor needs long numbers verification

--- RSAEncryptor/Encryptors/RSAEncryptor.cs
+++ RSAEncryptor/Encryptors/RSAEncryptor.cs
@@ -7,10 +7,81 @@ namespace Encryptors.Encryptors
 {
     public class RSAEncryptor : IEncryptor
     {
+        #region Variables
+        int n = 0;
+        int fi = 0;
+        int P = 0;
+        int Q = 0;
+        int e = 0;
+        int d = 0;
+        int[] PrimeNumbers = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+        public void SetVariables(int p, int q)
+        {
+            P = p;
+            Q = q;
+            n = P * Q;
+            fi = (P - 1) * (Q - 1);
+            Random rnd = new Random();
+            e = PrimeNumbers[rnd.Next(0, 25)];
+            while((e > fi) || (fi%e == 0))
+            {
+                e = PrimeNumbers[rnd.Next(0, 25)];
+            }
+            e = 17;
+            d = EuclidesAlgorithm();
+        }
+        #endregion
+
+        public int EuclidesAlgorithm()
+        {
+            int k = fi;
+            int X = e;
+            int Y = fi;
+            int Z = 1;
+            int aux = 0;
+            while (X != 1)
+            {
+                aux = Z;
+                Z = Y - ((k / X) * Z);
+                Y = aux;
+                if (Z < 0)
+                {
+                    Z = Z + fi;
+                }
+                aux = X;
+                X = k - ((k/X) * X);
+                k = aux;
+            }
+            return Z;
+        }
         public string[] GetKeys(string p, string q)
         {
             return new string[] { string.Empty, string.Empty };
         }
+        public string EncryptString(int p, int q, string message)
+        {
+            SetVariables(p, q);
+            string emessage = "";
+            byte[] chara = new byte[1];
+            foreach (var c in message)
+            {
+                chara[0]= Convert.ToByte((Convert.ToInt32(Convert.ToByte(c))^ e) % n);
+                emessage += Convert.ToBase64String(chara);
+            }
+            return emessage;
+        }
+        public string DecryptString(string cmessage)
+        {
+            string message = "";
+            byte[] chara = new byte[1];
+            foreach (var c in cmessage)
+            {
+                chara[0] = Convert.ToByte((Convert.ToInt32(Convert.ToByte(c)) ^ d) % n);
+                message += Convert.ToBase64String(chara);
+            }
+            return message;
+        }
+
 
         public string EncryptFile(string keyPath, string filePath, string savingPath, string nombre)
         {
--- StringEncryptor/Program.cs
+++ StringEncryptor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Encryptors;
+using Encryptors.Encryptors;
 
 namespace StringEncryptor
 {
@@ -6,7 +8,26 @@ namespace StringEncryptor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RSAEncryptor rSA = new RSAEncryptor();
+            Console.WriteLine("Escribe el string a cifrar");
+            string text = Console.ReadLine();
+            Console.WriteLine("Escribe la llave p");
+            string p = Console.ReadLine();
+            Console.WriteLine("Escribe la llave q");
+            string q = Console.ReadLine();
+            Console.WriteLine("Se ha guardado el string con éxito para cifrar");
+            Console.WriteLine("El resultado del cifrado es el siguiente:");
+            Console.WriteLine(rSA.EncryptString(Convert.ToInt32(p), Convert.ToInt32(q), text));
+            Console.WriteLine("¿Desea descifrarlo? | Presione 'Y'. De lo contrario, presione cualquier otra tecla.");
+            if (Console.ReadKey().Key == ConsoleKey.Y)
+            {
+                Console.Clear();
+                Console.WriteLine("El resultado del descifrado es el siguiente:");
+                Console.WriteLine(rSA.DecryptString(text));
+                Console.ReadLine();
+            }
+            Console.WriteLine("Feliz día!");
+            Console.ReadLine();
         }
     }
 }
